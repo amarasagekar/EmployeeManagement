4ccf900e64bd28719088dfcc8f195199315a63fd
Amar Asagekar <amarasagekar@gmail.com>
2024-04-02 22:07:53 +0530

    employe list and attendance

--- employee-mangement/app/Home/index.js
+++ employee-mangement/app/Home/index.js
@@ -1,11 +1,89 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
 import React from "react";
-
+import { LinearGradient } from "expo-linear-gradient";
+import { Feather, Entypo, Ionicons } from "@expo/vector-icons";
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <ScrollView>
+      <LinearGradient colors={["#7E7FD5", "#E9E4F0"]} styles={{ flex: 1 }}>
+        <View style={{ padding: 12 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Feather name="bar-chart" size={24} color="black" />
+            <Text style={{ fontSize: 16, fontWeight: "600" }}>
+              Employee Managemanet System
+            </Text>
+            <Entypo name="lock" size={24} color="black" />
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 20,
+          }}
+        >
+          <Pressable
+            style={{
+              backgroundColor: "#D3CCE3",
+              padding: 12,
+              borderRadius: 6,
+              alignItems: "center",
+              justifyContent: "center",
+              flex: 1,
+            }}
+          >
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Ionicons name="people-sharp" size={24} color="black" />
+            </View>
+            <Text style={{ marginTop: 7, fontWeight: "600" }}>
+              Employee List
+            </Text>
+          </Pressable>
+          <Pressable
+            style={{
+              backgroundColor: "#D3CCE3",
+              padding: 12,
+              borderRadius: 6,
+              alignItems: "center",
+              justifyContent: "center",
+              flex: 1,
+            }}
+          >
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Ionicons name="people-sharp" size={24} color="black" />
+            </View>
+            <Text style={{ marginTop: 7, fontWeight: "600" }}>
+              Mark Attendance
+            </Text>
+          </Pressable>
+        </View>
+      </LinearGradient>
+    </ScrollView>
   );
 };
 
