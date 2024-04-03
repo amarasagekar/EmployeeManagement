960fff47c17f3836523334304ab7dbee0fbe1685
Amar Asagekar <amarasagekar@gmail.com>
2024-04-03 16:25:38 +0530

    addedd attendance reaport tab

--- employee-mangement/app/Home/index.js
+++ employee-mangement/app/Home/index.js
@@ -82,6 +82,62 @@ const index = () => {
             </Text>
           </Pressable>
         </View>
+        <View
+          style={{
+            marginTop: 20,
+            backgroundColor: "white",
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+            borderRadius: 7,
+          }}
+        >
+          <Pressable
+            style={{
+              backgroundColor: "#BE93C5",
+              borderRadius: 6,
+              padding: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 10,
+            }}
+          >
+            <View
+              style={{
+                padding: 7,
+                width: 45,
+                height: 45,
+                borderRadius: 7,
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Ionicons name="newspaper-outline" size={24} color="black" />
+            </View>
+            <Text
+              style={{
+                marginLeft: 10,
+                fontSize: 16,
+                fontWeight: "600",
+                flex: 1,
+              }}
+            >
+              Attendance Report
+            </Text>
+            <View
+              style={{
+                width: 35,
+                height: 35,
+                borderRadius: 7,
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Entypo name="chevron-right" size={24} color="black" />
+            </View>
+          </Pressable>
+        </View>
       </LinearGradient>
     </ScrollView>
   );
