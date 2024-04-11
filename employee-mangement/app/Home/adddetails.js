55e2e9b959c327b14f94441f17fdd6cac212d01f
Amar Asagekar <amarasagekar@gmail.com>
2024-04-11 22:49:31 +0530

    employee detail screen

--- employee-mangement/app/Home/adddetails.js
+++ employee-mangement/app/Home/adddetails.js
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, TextInput } from "react-native";
+import React from "react";
 
 const adddetails = () => {
   return (
-    <ScrollView>
-      <Text>adddetails</Text>
+    <ScrollView style={{ flex: 1, backgroundColor: "White" }}>
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontSize: 17, fontWeight: "bold" }}>
+          Add a New Employee
+        </Text>
+
+        <TextInput
+          style={{
+            padding: 10,
+            borderColor: "#D0D0D0",
+            borderWidth: 1,
+            marginTop: 10,
+            borderRadius: 5,
+          }}
+          placeholder="India"
+          placeholderTextColor={"black"}
+        />
+
+        <View style={{ marginVertical: 10 }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold" }}>
+            Full Name (First and last Name)
+          </Text>
+          <TextInput
+            style={{
+              padding: 10,
+              borderColor: "#D0D0D0",
+              borderWidth: 1,
+              marginTop: 10,
+              borderRadius: 5,
+            }}
+            placeholder="Enter your name"
+            placeholderTextColor={"black"}
+          />
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 17, fontWeight: "bold" }}>Employee Id</Text>
+          <TextInput
+            style={{
+              padding: 10,
+              borderColor: "#D0D0D0",
+              borderWidth: 1,
+              marginTop: 10,
+              borderRadius: 5,
+            }}
+            placeholder="Employee Id"
+            placeholderTextColor={"black"}
+          />
+        </View>
+      </View>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default adddetails
+export default adddetails;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
